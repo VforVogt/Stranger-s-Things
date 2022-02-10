571062b5b4ce524c4279d0b72c9d68ade647ea83
VforVogt <ChrisVGT@outlook.com>
2022-02-09 23:41:38 -0600

    New content, just pick up from the bottom of App.js

--- src/App.js
+++ src/App.js
@@ -23,10 +23,96 @@ export default function App() {
 
     const getPosts = async () => {
       try {
-        const response = await fetch(`${API_URL}`);
+        const response = await fetch(`${API_URL}/posts`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const { data } = await response.json();
+
+        setPosts(data.posts.reverse());
       } catch (err) {
-        throw err;
+        console.log(err);
       }
     };
-  });
+    getPosts();
+  }, [me]);
+
+  const [meFlag, setMeFlag] = useState(false);
+
+  useEffect(() => {
+    const getMe = async () => {
+      try {
+        const response = await fetch(`${API_URL}/users/me`, {
+          headers: {
+            "Content Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const { data: me } = await response.json();
+
+        setMe(me);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }, [token, meFlag]);
+
+  const updateMe = () => {
+    setMeFlag(!meFlag);
+  };
+
+  return (
+    <Router>
+      <main>
+        <Nav
+          isLogged={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          setMe={setMe}
+        />
+        <Route
+          exact
+          path={"/"}
+          render={() => (
+            <Posts posts={posts} setPosts={setPosts} isLoggedIn={isLoggedIn} />
+          )}
+        />
+        <Route
+          exact
+          path={"/home"}
+          render={() => (
+            <Posts posts={posts} setPosts={setPosts} isLoggedIn={isLoggedIn} />
+          )}
+        />
+        {!isLoggedIn && (
+          <Switch>
+            <Route
+              exact
+              path={"/login"}
+              render={() => (
+                <LoginOrRegister
+                  setToken={setToken}
+                  setIsLoggedIn={setIsLoggedIn}
+                />
+              )}
+            />
+            <Route
+              exact
+              path={"/register"}
+              render={() => (
+                <LoginOrRegister
+                  setToken={setToken}
+                  setIsLoggedIn={setIsLoggedIn}
+                />
+              )}
+            />
+          </Switch>
+        )}
+        {isLoggedIn && <Switch></Switch>}
+      </main>
+    </Router>
+  );
 }
