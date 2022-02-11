cfdcb86f9a563a1d631a42b212f41d5262651539
VforVogt <ChrisVGT@outlook.com>
2022-02-10 18:37:31 -0600

    New compnent files created. Content incoming.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Nav } from "./Nav";
+export { default as Posts } from "./Posts";
+export { default as CreatePost } from "./CreatePost";
+export { default as LoginOrRegister } from "./LoginOrRegister";
+export { default as Me } from "./Me";
+export { default as CreateMessage } from "./CreateMessage";
